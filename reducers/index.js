8c33b25d0b845afba57f0e570c85256a74af21cf
davislurve <davidajberner@gmail.com>
2016-01-11 10:58:12 +1100

    More counter removal

--- reducers/index.js
+++ reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
-import counter from './counter'
 import segmentBuilder from './segmentbuilder'
 
 const rootReducer = combineReducers({
-  counter,
   segmentBuilder
 })
 
