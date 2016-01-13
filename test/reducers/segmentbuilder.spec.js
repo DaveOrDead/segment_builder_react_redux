4d9ce205481974d82f5fed7ea5f877c6319427f6
davislurve <davidajberner@gmail.com>
2016-01-13 15:13:34 +1100

    Unit tests for ConditionButton and Segment Builder Reducer

--- test/reducers/segmentbuilder.spec.js
+++ test/reducers/segmentbuilder.spec.js
@@ -8,5 +8,24 @@ describe('reducers', () => {
     it('should handle initial state', () => {
       expect(segmentbuilder(undefined, {})).toBe(initialState)
     })
+
+    it('should handle ADD_RULE', () => {
+      let addRuleAction =  {
+        type: ADD_RULE,
+         ruleGroupIndex: 0
+       }
+       let expectedRulesForFirstGroup = {
+            Rules : [
+                {
+                    id: '0',
+                    ruleTypeId: 'default',
+                    ruleQualifierId: null,
+                    ruleCriteria: '',
+                    ruleGroupIndex: 0
+                }
+            ]
+       }
+       expect(segmentbuilder(initialState, addRuleAction).RuleGroups[0]).toEqual(expectedRulesForFirstGroup)
+    })
   })
 })
